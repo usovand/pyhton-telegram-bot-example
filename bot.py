3aaf642f5f5fa5ff7b7ccfc296fc52dd451101d1
Andrei Usov <usovand@gmail.com>
2018-05-10 13:52:43 +0300

    Async

--- bot.py
+++ bot.py
@@ -1,46 +1,41 @@
-import flask
-import requests
+import asyncio
+import aiohttp
 
-from threading import Thread
+from aiohttp import web
+from urllib.parse import urljoin
 
-TOKEN = 'token'
-BOT_API_URL = 'https://api.telegram.org/bot' + TOKEN + '/{method}'
-
-WEBHOOK_PORT = 8081
-WEBHOOK_URL = '167.99.214.149:' + str(WEBHOOK_PORT) + '/' + TOKEN
-
-SERVER = flask.Flask(__name__)
+from config.host import *
 
 
-@SERVER.route('/' + TOKEN, methods=['POST'])
-def webhook():
-    data = flask.request.get_data().decode('utf-8')
-    Thread(target=run, kwargs={'update_data': data}).start()
+async def handle_updates(request):
+    body = await request.json()
+    asyncio.ensure_future(process_updates(body))
 
-    return flask.Response(status=200)
-
-
-def send_api_request(method: str, data=None) -> requests.Response:
-    """
-    Send request to telegram bot api
-
-    :param method: api method
-    :param data: additional data
-    :return: api response
-    """
-    if data is None:
-        data = {}
-
-    r = requests.post(BOT_API_URL.format(method=method), json=data)
-
-    return r
+    return web.Response()
 
 
-def run(update_data):
-    print(update_data)
+async def process_updates(updates):
+    print(updates)
+
+
+async def send_api_request(method, json=None):
+    if json is None:
+        json = {}
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(urljoin(API_URL, method), json=json) as resp:
+            return resp
+
+
+def run_updates_handler():
+    app = web.Application()
+    app.add_routes([web.post('/' + TOKEN, handle_updates)])
+
+    web.run_app(app)
 
 
 if __name__ == '__main__':
-    send_api_request('setWebhook', {'url': WEBHOOK_URL})
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(send_api_request('setWebhook', json={'url': WEBHOOK_URL}))
 
-    SERVER.run(host='0.0.0.0', port=WEBHOOK_PORT)
+    run_updates_handler()
---
+++ config/host.py
@@ -0,0 +1,11 @@
+# Api token
+TOKEN = 'token'
+
+# Api url
+API_URL = 'https://api.telegram.org/bot' + TOKEN + '/{method}'
+
+# Local server port
+PORT = 8080
+
+# Webhook url
+WEBHOOK_URL = 'https://bot.nixdrey.com/' + TOKEN
